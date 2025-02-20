2aa0ceeffaeea8e8ead6cdca28087378eb1d1b65
Danushri Prakash <danushri.saranyaprakash@gmail.com>
2025-02-20 14:59:09 +0530

    done

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const encrypt = (payload) => {
-  // encrypt the payload and return token
+const jwt = require('jsonwebtoken');
+const CryptoJS = require('crypto-js');
+
+const SECRET_KEY = "your_secret_key_here";
+const ENCRYPTION_KEY = "your_encryption_key_here";
+
+function encryptJWT(payload) {
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+    return CryptoJS.AES.encrypt(token, ENCRYPTION_KEY).toString();
 }
 
-const decrypt = (token) => {
-  // return decoded payload
+function decryptJWT(encryptedToken) {
+    try {
+        const bytes = CryptoJS.AES.decrypt(encryptedToken, ENCRYPTION_KEY);
+        const token = bytes.toString(CryptoJS.enc.Utf8);
+        return jwt.verify(token, SECRET_KEY);
+    } catch {
+        return null;
+    }
 }
 
-module.exports = {
-  encrypt,
-  decrypt
-}
+const payload = { userId: 123, username: 'Danu' };
+const encryptedJWT = encryptJWT(payload);
+console.log('Encrypted:', encryptedJWT);
+console.log('Decrypted:', decryptJWT(encryptedJWT));
